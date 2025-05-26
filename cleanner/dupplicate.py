5726579cb82707793bad29588e2141d40c614705
Bishoy Zakhary <bis.zakhary@stud.uniroma3.it>
2025-05-26 18:32:04 +0200

    provas

--- cleanner/dupplicate.py
+++ cleanner/dupplicate.py
@@ -21,6 +21,7 @@ def process_csv(file_path):
     output_file_path = file_path.replace("_pulito.csv", "_double.csv")
     df_modified.to_csv(output_file_path, index=False)
     print(f"File salvato come {output_file_path}")
+    print("ciao")
 
 
 process_csv("/Users/bishoyzakhary/Documents/GitHub/BigData_USED-CAR/used_cars_data.csv")
